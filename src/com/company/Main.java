f1e5bba0ff7e08fab969a58949fa1658d62d339f
Tlenc <tlenciauskas@gmail.com>
2018-01-31 21:36:12 +0200

    Code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Masyvo dydis : ");
+        int dyd = sc.nextInt();
+        int[] masyvas = new int[dyd];
+
+        System.out.println("Įveskite skaičius ");
+        for (int i = 0; i < masyvas.length; i++) {
+            masyvas[i] = sc.nextInt();
+        }
+        skaiciavimas(masyvas);
+    }
+
+
+    public static void skaiciavimas(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 100) {
+                System.out.println(array[i]);
+            }
+
+        }
+
     }
 }
